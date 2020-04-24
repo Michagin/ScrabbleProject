9ba7b5a974478cb29ce5eb89d12f4ec8e3e30404
Arrowstormen <arrowstorm.videos@gmail.com>
2020-04-24 13:28:02 +0200

    Made it so word is turned into char int list format

--- Cheesecake/Blueberry.fs
+++ Cheesecake/Blueberry.fs
@@ -46,17 +46,47 @@ let lookupWild (s:string) (dict:Dictionary) =
  let rec aux (s:string) dict (alphabet:char list) =
   match alphabet with 
    | [] -> ""
-   | x::xs -> if (lookup (s.Replace('.',x)) dict) then s.Replace('.',x) else aux s dict xs
+   | x::xs -> if (lookup (s.Replace('.',x)) dict) then s.Replace('.',x) else aux s dict xs 
  aux s dict (alphabet dict)
  
-let findValidWord (dict:Dictionary) (list: string list)  : string =
+let findValidWord (dict:Dictionary) (list: string list) =
  let rec aux list dict =
   match list with
   | [] -> ""
-  | (x:string)::xs when x.Contains('.') -> if (lookupWild x dict).Length > 0 then lookupWild x dict else aux xs dict 
+  | (x:string)::xs when x.Contains(".") -> if (lookupWild x dict).Length > 0 then lookupWild x dict else aux xs dict 
   | (x:string)::xs -> if (lookup x dict) then x else aux xs dict 
  aux list dict  
  
+ // from Stack Overflow https://stackoverflow.com/questions/2889961/f-insert-remove-item-from-list
+let rec insert v i l =
+     match i, l with
+     | 0, xs -> v::xs
+     | i, x::xs -> x::insert v (i - 1) xs
+     | i, [] -> failwith "index out of range"
+ 
+ // from Stack Overflow https://stackoverflow.com/questions/2889961/f-insert-remove-item-from-list
+let rec remove i l =
+     match i, l with
+     | 0, x::xs -> xs
+     | i, x::xs -> x::remove (i - 1) xs
+     | i, [] -> failwith "index out of range"
+
+let rec addPoints (hand:(char * int) list) i char =
+ match hand with
+ | [] -> (char,0),i 
+ | (c,p) :: xs when c = char -> (c,p),i
+ | (c,p) :: xs -> addPoints xs (i+1) char
+
+let addPointsToList hand list =
+ let rec aux hand list acc = 
+  match Seq.toList list with
+  | [] -> List.rev acc
+  | x :: xs -> 
+               let (cp,i) = addPoints hand 0 x
+               if i > (hand.Length-1) then aux hand xs (cp :: acc) else 
+               aux (remove i hand) xs (cp :: acc)
+ aux hand list []
+
 let decideWord (dict:Dictionary) (hand:(char * int) list) = 
  let rec theWord dict hand length = 
   match length with
@@ -64,6 +94,8 @@ let decideWord (dict:Dictionary) (hand:(char * int) list) =
    | x -> 
           let res = onlyLetters hand |> getPerms x |> lettersToWords |>  findValidWord dict
           if (res = "") then theWord dict hand (length-1) else res
- theWord dict hand 7 
-  
-   
+ Seq.toList (theWord dict hand 7) |> addPointsToList hand 
+
+let test = [('D',2);('V',2);('A',2);('C',2);('D',2);('.',0);('.',0)]
+
+let test2 = ['A';'V';'A';'C';'A';'D';'O']
