5ed8b46bf7442021193498969c07ddbf5b6be27d
Arrowstormen <arrowstorm.videos@gmail.com>
2020-04-24 15:16:04 +0200

    Can now handle multiple wild tiles

--- Cheesecake/Blueberry.fs
+++ Cheesecake/Blueberry.fs
@@ -41,12 +41,23 @@ let getPerms n lst =
 
 let lettersToWords (list:char list list) = (List.map (fun x -> System.String.Concat (x:char list)) list) 
 
-// Does not handle multiple wild tiles correctly
+let replaceFirstOcc char repchar (string:string) =
+ let rec aux char repchar (charlist:char list) (acc:char list) =
+   match charlist with
+   | [] -> System.String.Concat (List.rev acc )
+   | x::xs when x = char -> System.String.Concat (List.append (List.rev acc)(repchar::xs))
+   | x::xs -> aux char repchar xs (x::acc)
+ aux char repchar (Seq.toList string) []
+
 let lookupWild (s:string) (dict:Dictionary) = 
- let rec aux (s:string) dict (alphabet:char list) =
-  match alphabet with 
+ let rec aux (s:string) dict (alpha:char list) =
+  match alpha with 
    | [] -> ""
-   | x::xs -> if (lookup (s.Replace('.',x)) dict) then s.Replace('.',x) else aux s dict xs 
+   | x::xs when (lookup (s.Replace('.',x)) dict) -> s.Replace('.',x)
+   | x::xs when (replaceFirstOcc '.' x s).Contains(".") ->
+                                                            let string = aux (replaceFirstOcc '.' x s) dict (alphabet dict)
+                                                            if string.Length > 0 then string else aux s dict xs
+   | _::xs -> aux s dict xs
  aux s dict (alphabet dict)
  
 let findValidWord (dict:Dictionary) (list: string list) =
@@ -56,13 +67,6 @@ let findValidWord (dict:Dictionary) (list: string list) =
   | (x:string)::xs when x.Contains(".") -> if (lookupWild x dict).Length > 0 then lookupWild x dict else aux xs dict 
   | (x:string)::xs -> if (lookup x dict) then x else aux xs dict 
  aux list dict  
- 
- // from Stack Overflow https://stackoverflow.com/questions/2889961/f-insert-remove-item-from-list
-let rec insert v i l =
-     match i, l with
-     | 0, xs -> v::xs
-     | i, x::xs -> x::insert v (i - 1) xs
-     | i, [] -> failwith "index out of range"
  
  // from Stack Overflow https://stackoverflow.com/questions/2889961/f-insert-remove-item-from-list
 let rec remove i l =
@@ -79,7 +83,7 @@ let rec addPoints (hand:(char * int) list) i char =
 
 let addPointsToList hand list =
  let rec aux hand list acc = 
-  match Seq.toList list with
+  match list with
   | [] -> List.rev acc
   | x :: xs -> 
                let (cp,i) = addPoints hand 0 x
@@ -96,6 +100,6 @@ let decideWord (dict:Dictionary) (hand:(char * int) list) =
           if (res = "") then theWord dict hand (length-1) else res
  Seq.toList (theWord dict hand 7) |> addPointsToList hand 
 
-let test = [('D',2);('V',2);('A',2);('C',2);('D',2);('.',0);('.',0)]
+let test = [('A',2);('O',2);('V',2);('C',2);('D',2);('.',0);('.',0)]
 
-let test2 = ['A';'V';'A';'C';'A';'D';'O']
+let test2 = ['A';'V';'O';'C';'A';'D';'O']
